3939f7a9b134b01bce3761275c7fa398c13799c9
Lucas Sodré <lukassodre21@gmail.com>
2022-10-19 17:05:17 -0300

    Update db

--- src/a.js
+++ src/a.js
@@ -1,6 +1,9 @@
 const sqlite3 = require('sqlite3').verbose();
 const os = require("os");
 const fs = require("fs");
+const express = require("express");
+
+const app = express();
 
 let memCalc = (mem)=> {
 
@@ -16,20 +19,33 @@ let sistema = os.platform();
 let versao = os.version();
 let logon = os.userInfo().username;
 let hostname = os.hostname();
+const ipaddress = os.networkInterfaces().Ethernet.find(element =>{
 
-let texto = processador + memoria + sistema;
+    if (element.family === 'IPv4') {
+        
+        return element
+    } 
+});
+
+let ipv4 = ipaddress.address;
+let mac= ipaddress.mac;
 
 const db = new sqlite3.Database('database.sqlite');
 
 db.serialize(() => {
-    db.run("CREATE TABLE inventario (logon TEXT, hostname TEXT, processador TEXT, memoria TEXT, sistema TEXT, versao TEXTE)");
+    db.run("CREATE TABLE inventario (logon TEXT, hostname TEXT, processador TEXT, memoria INTEGER, sistema TEXT, versao TEXTE, ip TEXT, mac TEXT)");
 
-    const stmt = db.prepare("INSERT INTO inventario VALUES (?,?,?,?,?,?)");
+    const stmt = db.prepare("INSERT INTO inventario VALUES (?,?,?,?,?,?,?,?)");
 
-        stmt.run(logon, hostname, processador, memoria, sistema, versao)
+        stmt.run(logon, hostname, processador, memoria, sistema, versao, ipv4, mac)
     
         stmt.finalize();
     })
     
 db.close();
 // Hostname; Nome de Logon; Endereço IP; Version; 
+
+app.get("ifconfig.me/all.json", (req, res)=>{
+    req.body;
+})
+
--- src/teste.js
+++ src/teste.js
@@ -1,5 +1,17 @@
-const os = require('os');
+import os from "os";
+import geoip  from "geoip-lite";
+import express from "express";
+import fetch from "node-fetch";
 
 
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
-console.log(os.userInfo().username)
+const ipaddress = os.networkInterfaces().Ethernet.find(element =>{
+
+    if (element.family === 'IPv4') {
+        
+        return element
+    } 
+})
